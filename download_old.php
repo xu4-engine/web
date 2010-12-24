c408c743b441914925fcb52921009df7b948be58
Darren Janeczek <darren_janeczek@users.sourceforge.net>
2010-12-24 04:39:58 +0000

    Updated some files that were on the website. Not fully sync'd between web and svn. Still some CVS artifacts on web. Will leave them alone for now, and upload the new index to web.

---
+++ download_old.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>xu4 Download</title>
+  <link rel="stylesheet" type="text/css" href="css/xu4.css" />
+  <link rel="top" href="http://xu4.sourceforge.net/" title="xu4" />
+</head>
+<body>
+<div class="title"><h3><span class="heading">xu4 Download</span></h3></div>
+<div class="menu">
+  <span class="menuitem"><a accesskey="1" href="index.php">Home</a></span> |
+  <span class="menuitem"><a accesskey="2" href="http://sourceforge.net/projects/xu4">Sourceforge project page</a></span> |
+  <span class="menuitem"><a accesskey="3" href="screenshots.html">Screenshots</a></span> |
+  <span class="menuitem"><a accesskey="4" href="forum">Discussion Forum</a></span> |
+  <span class="menuitem"><a accesskey="5" href="faq.html">FAQ</a></span> |
+  <span class="selectedmenuitem">Download</span> |
+  <span class="menuitem"><a accesskey="7" href="links.html">Links</a></span>
+</div>
+<div class="section">
+  <span class="heading">xu4 Releases</span><br />
+  <p>Downloads are hosted with SourceForge.net. If you have a supported system, you can download the appropriate binary distribution.  If you have another system, download the source and read the README file for build directions.</p>
+  <p>Please <a href="http://xu4.sourceforge.net/forum/">let us know</a> if you successfully port xu4 to a new platform!</p>
+  <ul>
+    <li><a href="http://prdownloads.sourceforge.net/xu4/xu4-1.0beta3-win32.exe?download">xu4 1.0beta3 for Windows</a> <span class="details">(1.1Mb, Windows .EXE, doesn't include ultima4.zip, download below)</span></li>
+    <li><a href="http://prdownloads.sourceforge.net/xu4/xu4-1.0beta3-1.i386.rpm?download">xu4 1.0beta3 for Linux</a> <span class="details">(2.4Mb, Fedora Core 4 RPM)</span></li>
+    <li><a href="http://prdownloads.sourceforge.net/xu4/xu4-1.0beta3.dmg?download">xu4 1.0beta3 for Mac OS X</a> <span class="details">(3 Mb, OSX disk image)</span></li>
+    <li><a href="http://prdownloads.sourceforge.net/xu4/xu4-1.0beta3.tar.gz?download">xu4 1.0beta3 source code</a> <span class="details">(409Kb, gzipped tarfile)</span></li>
+    <li><a href="http://sourceforge.net/project/showfiles.php?group_id=36225">Other formats and older releases</a></li>
+  </ul>
+  <p>Offsite ports:</p>
+  <ul>
+    <li><a href="http://inter.zon.free.fr/zaurus_index.html#xu4">xu4 for the Sharp Zaurus Handheld</a></li>
+    <li><a href="http://www.beemulated.net/engine/origin.phtml">xu4 for BeOS</a></li>
+    <li><a href="http://www.imrtech.mirrorz.com/">xu4 for the Sega Dreamcast console</a></li>
+  </ul>
+</div>
+<div class="section">
+  <span class="heading">xu4 Daily Snapshot Builds</span><br /><br />
+  <ul>
+    <li><a href="download/xu4win32.exe">xu4 snapshot for Windows</a> <span class="details">(<? $statinfo = stat("download/xu4win32.exe"); echo date("Y-m-d G:i T", $statinfo[9]); ?>)</span></li>
+    <li><a href="download/xu4-MacOSX.dmg">xu4 snapshot for Mac OS X</a> <span class="details">(<? $statinfo = stat("download/xu4-MacOSX.dmg"); echo date("Y-m-d G:i T", $statinfo[9]); ?>, Mac OS X version 10.3.9 or later required)</span></li>
+  </ul>
+</div>
+<div class="section">
+  <span class="heading">Ultima IV for DOS</span><br />
+  <p><s>Ultima IV for DOS is <strong>required</strong> to play xu4</s>.  Ultima IV for DOS is now included in the Linux and Mac OS X binary downloads.  You still need it if you use Windows or compile from source.  Origin generously allows it to be freely distributed; it is mirrored here.  The graphics upgrade is optional.</p>
+  <ul>
+    <li><a href="http://prdownloads.sourceforge.net/xu4/ultima4-1.01.zip?download">Ultima IV for DOS</a></li>
+    <li><a href="http://prdownloads.sourceforge.net/xu4/u4upgrad.zip?download">The Ultima IV Graphics upgrade</a> (<a href="http://www.moongates.com/u4/upgrade/Upgrade.htm">homepage</a>)</li>
+  </ul>
+</div>
+<div class="section">
+  <span class="heading">xu4 Developer Downloads</span><br />
+  <p>Xu4 downloads for developers.</p>
+  <ul>
+    <li><a href="download/xu4_dev_win32.zip">MinGW headers and libraries</a></li>
+    <li><a href="download/xu4_dev_win32_VC6.zip">MS Visual C++ headers and libraries</a></li>
+  </ul>
+</div>
+  
+<div class="footer" id="host">
+  Hosted at&nbsp;
+  <a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=36225&type=5" width="210" height="62" alt="SourceForge Logo" /></a> 
+</div>
+
+<div class="footer" id="w3">
+  <a href="http://validator.w3.org/check/referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0!" height="31" width="88" /></a>
+</div>
+
+</body>
+</html>
